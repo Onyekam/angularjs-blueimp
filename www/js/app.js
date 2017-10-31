83363ce2fe34749ea2ad0089428ab7c19564e93b
Michael <michaelo_@hotmail.com>
2017-10-31 07:06:07 +0100

    video now embedding

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,20 @@
 (function() {
     'use strict';
 
+    function getHash(hash) {
+        var Video = hash;
+    }
+
     function UploaderController() {
         this.wistia = {
             url: 'https://upload.wistia.com',
             mediaUrl: 'https://api.wistia.com/v1/medias.json',
             params: {
+                //api_password: '9ad6e65b0245ba343f7fd1f1dc7eb4800f3127abe9f20a2d7a84b5cd248fc8f'
                 api_password: '9ad6e65b0245ba343f7fd1f1dc7eb4800f3127abe9f20a2d7a84b5cd248fc8f8'
             }
         };
+        //this.uploadedVideo2 = { hashedId: "michael" };
         $('#fileupload').fileupload({
             url: this.wistia.url,
             autoUpload: true,
@@ -18,16 +24,20 @@
                 name: 'api_password',
                 value: this.wistia.params.api_password
             }],
-            done: function(e, data) {
-                var hashedId = data.result.hashed_id,
-                    params = {
-                        api_password: this.wistia.params.api_password,
-                        hashed_id: hashedId
-                    };
-                $http.get(this.wistia.mediaUrl, { params }).then(success, error);
+            success: function(data, e) {
+                var uploadedVideo = {
+                    hashedId: data.hashed_id
+                };
+                console.log('<script src="https://fast.wistia.com/embed/medias/' + uploadedVideo.hashedId + '.jsonp" async></script> <script src = "https://fast.wistia.com/assets/external/E-v1.js"async > < /script><span class="wistia_embed wistia_async_' + uploadedVideo.hashedId + ' popover=true popoverAnimateThumbnail=true" style="display:inline-block;height:84px;width:150px">&nbsp;</span > ');
+                var videoembed = angular.element('<script src="https://fast.wistia.com/embed/medias/' + uploadedVideo.hashedId + '.jsonp" async></script> <script src = "https://fast.wistia.com/assets/external/E-v1.js"async></script><span class="wistia_embed wistia_async_' + uploadedVideo.hashedId + ' popover=true popoverAnimateThumbnail=true" style="display:inline-block;height:168px;width:300px">&nbsp;</span > ');
+                var embeddiv = angular.element(document).find('#videoembed').eq(0);
+                embeddiv.append(videoembed);
+                var mediaUrl = 'https://api.wistia.com/v1/medias.json';
+                // angular.element(videoembed).appendTo('#fileupload');
+                //return this.uploadedVideo;
             }
-
         });
+
     }
     angular.module('angularJSTest', [
             'blueimp.fileupload'
@@ -35,7 +45,6 @@
         .component('wistiaUploader', {
             templateUrl: 'js/uploader.html',
             controller: UploaderController
-
         })
         .config([
             '$httpProvider', 'fileUploadProvider',
